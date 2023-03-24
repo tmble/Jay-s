0ede42694f10e7296d6eb277d2cf1c5635449025
tmble <128164838+tmble@users.noreply.github.com>
2023-03-24 22:09:32 +0800

    Add files via upload

---
+++ 6.3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	printf("%lf/n", pow(4, 4));
+	return 0;
+}
---
+++ 6.5.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	printf("%lf/n", sqrt(25));
+	return 0;
+}
