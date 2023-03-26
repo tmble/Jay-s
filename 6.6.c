d0573bb51082a4eb0060e184e7b52fe12fb3806a
tmble <128164838+tmble@users.noreply.github.com>
2023-03-26 18:21:04 +0800

    Add files via upload

---
+++ 6.6.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	printf("%lf/n", log2(16));
+	return 0;
+}
---
+++ 6.7.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	printf("%lf/n", log2(16));
+	return 0;
+
+}
