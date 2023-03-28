2aadfdc2b5ed7652eb67f59b3862c0d26c40d38b
tmble <128164838+tmble@users.noreply.github.com>
2023-03-28 22:14:54 +0800

    Add files via upload

---
+++ 6.12.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	printf("%lf/n", acos( cos(1)));
+	return 0;
+
+}
---
+++ 6.13.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	printf("%lf/n",sinh(1)/cosh(1)-tanh(1));
+	return 0;
+}
