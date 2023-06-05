1061d1269d44aa9a14817f9f0466ab00ddfd0055
tmble <128164838+tmble@users.noreply.github.com>
2023-06-05 19:43:53 +0800

    Add files via upload

---
+++ 狗叫.py
@@ -0,0 +1,8 @@
+print('hello\nworld')
+print('hello\tworld')
+print('helloooo\tworld')
+print('hello\rworld')
+print('hello\bworld')
+print('http:\\\\www.baidu.com')
+print('狗叫：\'叫\'')
+print(r'hello\nworld\\')
