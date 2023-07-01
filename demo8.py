e8cf548be9d10194e910ea53ca672ccb5dfa8968
tmble <128164838+tmble@users.noreply.github.com>
2023-07-01 22:23:41 +0800

    Add files via upload

--- demo8.py
+++ demo8.py
@@ -3,3 +3,10 @@ age=22
 
 print(type(name),type(age))
 print('我叫'+name+'，今年'+str(age)+'岁')
+s1='128.98'
+s2='76'
+ff=True
+s3='hello'
+i=98
+print(type(s1),type(s2),type(ff),type(s3),type(i))
+print(float(s1),type(float(s1)))
