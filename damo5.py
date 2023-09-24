3a108363da3268e8e51217db805463abf692fc7e
tmble <128164838+tmble@users.noreply.github.com>
2023-09-24 11:02:44 +0800

    Add files via upload

--- damo5.py
+++ damo5.py
@@ -1,9 +1,16 @@
-a=3.14159
-print(a,type(a))
-n1=1.1
-n2=2.2
-n3=2.1
-print(n1+n2)
-print(n1+n3)
-from decimal import Decimal
-print(Decimal('1.1')+Decimal('2.2'))
+a,b=10,2
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+a=5
+b=5
+print(a is b)
+lst1=[1,2,3,4]
+lst2=[1,2,3,4]
+print(lst1==lst2)
+print(lst1 is lst2)
+print(id(lst1))
+print(id(lst2))
+print(a is not b)
+print(lst1 is not lst2)
