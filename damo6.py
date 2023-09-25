a3a3e4e6f9dcd191d8a7b1e2752f002372761c2d
tmble <128164838+tmble@users.noreply.github.com>
2023-09-25 19:44:57 +0800

    Add files via upload

---
+++ damo6.py
@@ -0,0 +1,11 @@
+a,b=1,2
+print(a==1 and b==2)
+print(a!=2 and b!=1)
+print(a!=2 and b!=2)
+print(a==1 or b==2)
+print(a!=1 or b!=2)
+f=True
+print(not f)
+s='has'
+print('w' in s)
+print('w' not in s)
