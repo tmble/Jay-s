d6e3113e7571022f845361a5d5976daafcb1630c
tmble <128164838+tmble@users.noreply.github.com>
2023-09-29 16:59:19 +0800

    Add files via upload

--- damo3.py
+++ damo3.py
@@ -1,4 +1,5 @@
-print(9//4)
-print(9//-4)
-print(9%-4)
-print(-9%4)
+money=1000
+a=int(input('请输入取款金额'))
+if money>=a:
+    money=money-a
+    print('取款成功余额为：',money)
--- damo4.py
+++ damo4.py
@@ -1,6 +1,5 @@
-a=4
-a/=2
-print(a)
-a,b=11,22
-a,b=b,a
-print(a,b)
+l=int (input('请输入一个整数'))
+if l%2==0:
+          print(l,'是偶数')
+else:
+          print(l,'是奇数')
