51240b74ab464c1dd3ebee80e14762aea1327705
tmble <128164838+tmble@users.noreply.github.com>
2023-10-01 15:58:58 +0800

    Add files via upload

--- damo6.py
+++ damo6.py
@@ -1,11 +1,14 @@
-a,b=1,2
-print(a==1 and b==2)
-print(a!=2 and b!=1)
-print(a!=2 and b!=2)
-print(a==1 or b==2)
-print(a!=1 or b!=2)
-f=True
-print(not f)
-s='has'
-print('w' in s)
-print('w' not in s)
+answer=input('你是鹏子哥吗？yes/no')
+money=float(input('请输入购物金额'))
+if answer=='yes':
+    if money>=200:
+        print('买的不错，打八折，下次多买点：',money*0.8)
+    elif money>=100:
+        print('有点少哦，这次打九折：',money*0.9)
+    else:
+        print('买这么少，打你妹的折：',money)
+else:
+    if money>=200:
+        print('下次多买点,这次打九五折：',money*0.95)
+    else:
+        print('买这么少，还想打折：',money)
