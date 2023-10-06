12bb4705acda450386f7e88212bc52ee735046cd
tmble <128164838+tmble@users.noreply.github.com>
2023-10-06 11:14:18 +0800

    Delete 狗叫.py

--- 狗叫.py
+++
@@ -1,8 +0,0 @@
-print('hello\nworld')
-print('hello\tworld')
-print('helloooo\tworld')
-print('hello\rworld')
-print('hello\bworld')
-print('http:\\\\www.baidu.com')
-print('狗叫：\'叫\'')
-print(r'hello\nworld\\')
