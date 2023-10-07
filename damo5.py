97b61bb331767519e100dfe57bccbfefd67b044c
tmble <128164838+tmble@users.noreply.github.com>
2023-10-07 11:06:08 +0800

    Add files via upload

--- damo5.py
+++ damo5.py
@@ -1,9 +1,9 @@
-s=int(input('请输入一个成绩'))
-if 90<=s<=100:
-    print('A级')
-elif 60<=s<=89:
-    print('B级')
-elif 0<=s<=59:
-    print('C级')
-else:
-    print('你™会不会输')
+for item in range(1,21):
+    if item%5==0:
+        print(item)
+
+
+for item in range(1,51):
+    if item%5!=0:
+        continue
+    print(item)
