bcbb5d1e2d14159631da42337f4cd5cce73b3da9
tmble <128164838+tmble@users.noreply.github.com>
2023-10-09 08:59:40 +0800

    Add files via upload

---
+++ damo8.py
@@ -0,0 +1,9 @@
+for i in range(1,5):
+    for k in  range(1,5):
+        print('*',end='\t')
+    print()
+
+for i in range(1,10):
+    for l in range(1,i+1):
+        print('*',end='')
+    print()
--- damo9.py
+++ damo9.py
@@ -1,1 +1,4 @@
-print('hello')
+for i in range(1,10):
+    for u in range(1,i+1):
+        print(u,'*',i,'=',i*u,end='\t')
+    print()
