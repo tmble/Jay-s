a0036740eba3624fc856f3e052f99c271e61eff7
tmble <128164838+tmble@users.noreply.github.com>
2023-10-15 11:36:17 +0800

    Add files via upload

--- damo6.py
+++ damo6.py
@@ -1,9 +1,14 @@
-for item in range(3):
-    pwd=input('请输入密码:')
-    if pwd=='8888':
-        print('密码正确')
-        break
-    else:
-        print('密码不正确')
-else:
-    print('对不起你个老登')
+a='hello'
+j='world'
+print(a+j)
+print('-'*40)
+s='helloworld'
+print('e'in s)
+print('v'in s)
+print('e'not in s)
+print('v'not in s)
+print(len(s))
+print(max(s))
+print(min(s))
+print(s.index('o'))
+print(s.count('l'))
