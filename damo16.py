e98a721a6b0a07498940a78c801696da26a2fe33
tmble <128164838+tmble@users.noreply.github.com>
2023-10-22 11:37:49 +0800

    Add files via upload

--- damo16.py
+++ damo16.py
@@ -1,3 +1,24 @@
-s1='0518'
-print(type(s1))
-print(float(s1))
+d={10:'cat',20:'dog',30:'pet',20:'zoo'}
+print(d)#key相同时，value值进行了覆盖
+
+
+lst1=[10,20,30,40]
+lat2=['cat','dor','koh','huh','jij']
+a=zip(lst1,lat2)
+print(a)#<zip object at 0x000002F0E1767000>
+#print(list(a))#[(10, 'cat'), (20, 'dor'), (30, 'koh'), (40, 'huh')]
+d=dict(a)
+print(d)#{10: 'cat', 20: 'dor', 30: 'koh', 40: 'huh'}
+
+#使用参数创建字典
+d=dict(cat=10,dog=20)#左侧cat是key,右侧是value
+print(d)
+
+t=(10,20,30)
+print({t:10})#t是key,10是value,元组是可以作为字典中的key
+#字典属于序列
+print('max:',max(d))
+print('min:',min(d))
+print('len:',len(d))
+#del d
+print(d)
