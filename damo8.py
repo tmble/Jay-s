1030492ad42ac22eb3079e6a483885c492ea0190
tmble <128164838+tmble@users.noreply.github.com>
2023-11-04 13:31:21 +0800

    Add files via upload

--- damo8.py
+++ damo8.py
@@ -1,12 +1,20 @@
-lst=['hello','world','python','p2p']
-for item in lst:
-    print(item)
-for i in range(0,len(lst)):
-    print(i,'--->',lst[i])
+s='HelloWorld'
+#字符串的替换
+new_s=s.replace('o','嗯',1)#最后1一个参数是替换次数，默认是全部替换
+print(new_s)
 
-for index,item in enumerate(lst):
-    print(index,item)
-for index,item in enumerate(lst,start=66):
-    print(index,item)
-for index,item in enumerate(lst,99):
-    print(index, item)
+#字符串在指定的宽度范围内居中
+print(s.center(20))
+print(s.center(20,'*'))
+
+#去除字符串左右的空格
+s='     Hello     World     '
+print(s.strip())
+print(s.lstrip())
+print(s.rstrip())
+
+#去掉指定的字符
+s3='dl_HelloWorld'
+print(s3.strip('ld'))#与顺序无关
+print(s3.lstrip('ld'))
+print(s3.rstrip('ld'))
