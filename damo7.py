120a69dd883643df1e3e18e538a452bfe3ee992f
tmble <128164838+tmble@users.noreply.github.com>
2023-11-09 09:50:15 +0800

    Add files via upload

--- damo7.py
+++ damo7.py
@@ -1,16 +1,18 @@
-lst=['hello','world',98,797.1]
-print(lst)
-lst2=list('helloworld')
-print(lst2)
-lst3=list(range(1,11,2))
-print(lst3)
-print(lst+lst2+lst3)
-print(lst3*3)
-print(max(lst3))
-print(min(lst3))
-print(lst2.count('o'))
-print(lst2.index('o'))
-lst4=[10.5]
-print(lst4)
-del lst4
-#print(lst4)
+s1='hello'
+s2='world'
+#（1）使用+进行拼接
+print(s1+s2)
+
+#(2)使用字符串的join（）方法
+print(''.join([s1,s2]))#使用空字符串进行拼接
+
+print('*'.join(['hello','python','java','app']))
+print('你好'.join(['hello','java','app']))
+
+#(3)直接拼接
+print('ha''oi')
+
+#(4）使用格式化字符串进行拼接
+print('%s%s' % (s1,s2))
+print(f'{s1}{s2}')
+print('{0}{1}'.format(s1,s2))
