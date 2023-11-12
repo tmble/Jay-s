a7a52e85598c2546838972d3deb3740dc1bc8ac2
tmble <128164838+tmble@users.noreply.github.com>
2023-11-12 11:21:09 +0800

    Add files via upload

--- damo11.py
+++ damo11.py
@@ -1,9 +1,10 @@
-import random
-lst=[item for item in range(1,16)]
-print(lst)
-lst=[item*item for item in range(1,16)]
-print(lst)
-lst=[random.randint(1,100) for _ in range(1,16)]
-print(lst,id(lst))
-lst=[i for i in range(10) if i%2==0]
+import re
+pattern='黑客|破解|反爬'
+s='我想学习Python,想破解一些vip视频,Python可以实现无底线反爬吗？'
+new_s=re.sub(pattern,'***',s)
+print(new_s)
+
+s2='https://cn.bing.com/search?q=%E5%88%98%E7%9B%8A%E6%97%AD'
+pattern2='[?|%]'
+lst=re.split(pattern2,s2)
 print(lst)
