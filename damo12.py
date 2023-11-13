94edbaee9c5d3ff876f7e20b4c8745bb6a41e5b2
tmble <128164838+tmble@users.noreply.github.com>
2023-11-13 14:33:57 +0800

    Add files via upload

--- damo12.py
+++ damo12.py
@@ -1,17 +1,4 @@
-lst=[
-    ['城市','环比','同比'],
-    ['北京',102,103],
-    ['上海',101,135],
-    ['深圳',156,488]
-]
-print(lst)
-for row in lst:
-    for item in row:
-        print(item,end='\t')
-    print()
-lst2=[[j for j in range(5)]for i in range(4)]
-print(lst2)
-for row in lst2:
-    for item in row:
-        print(item,end='\t')
-    print()
+lst=['京A888888','津B66666','川A88888']
+for item in lst:
+    area=item[0:1]
+    print(item,'归属地为：',area)
