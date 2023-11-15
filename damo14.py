4084deddd8c3c1683d35cd5dab4d766dbc1affcb
tmble <128164838+tmble@users.noreply.github.com>
2023-11-15 11:24:10 +0800

    Add files via upload

--- damo14.py
+++ damo14.py
@@ -1,12 +1,20 @@
-t=('python','hello','world')
-print(t)
-t2=t[0:3:2]
-print(t2)
-for item in t:
-    print(item)
-for i in range(len(t)):
-    print(i,t[i])
-for index,item in enumerate(t):
-    print(index,'---->',item)
-for index,item in enumerate(t,start=99):
-    print(index,'---->',item)
+lst=[
+    ['01','鹏子哥的笔','九阳',500],
+    ['02','鹏子哥的本','美的',1000],
+    ['03','鹏子哥的电脑','联想',400],
+]
+print('编号\t\t名称\t\t\t\t品牌\t\t单价')
+for item in lst:
+    for i in item:
+        print(i,end='\t\t')
+    print()#换行
+#格式化
+for item in lst:
+    item[0]='0000'+item[0]
+    item[3]='￥{0:.2f}'.format(item[3])
+
+print('编号\t\t\t名称\t\t\t\t品牌\t\t单价')
+for item in lst:
+    for i in item:
+        print(i,end='\t\t')
+    print()#换行
