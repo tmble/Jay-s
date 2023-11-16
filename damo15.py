4ef8e22a30a7a4563266bae4e4e51c13d6df08bc
tmble <128164838+tmble@users.noreply.github.com>
2023-11-16 11:06:36 +0800

    Add files via upload

--- damo15.py
+++ damo15.py
@@ -1,12 +1,12 @@
-t=(i for i in range(1,4))
-print(t)
-#t=tuple(t)
-#print(t)
-#遍历
-#for item in t:
-#    print(item)
-print(t.__next__())
-print(t.__next__())
-print(t.__next__())
-t=tuple(t)
-print(t)
+lst=[
+    {'title':'肖申克的救赎','actors':['亚瑟.摩根']},
+]
+name=input('请输入你要查询的演员：')
+for item in lst:
+    #print(item)#字典
+    actors_lst=item.get('actors')
+
+    if name in actors_lst:
+       title=item.get('title')
+       print(title)
+       print(name,'出演了',title)
