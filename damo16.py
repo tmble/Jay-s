fbb6252dd665f3068b12afae784c9c8ef38e886b
tmble <128164838+tmble@users.noreply.github.com>
2023-11-17 18:53:40 +0800

    Add files via upload

--- damo16.py
+++ damo16.py
@@ -1,24 +1,11 @@
-d={10:'cat',20:'dog',30:'pet',20:'zoo'}
-print(d)#key相同时，value值进行了覆盖
-
-
-lst1=[10,20,30,40]
-lat2=['cat','dor','koh','huh','jij']
-a=zip(lst1,lat2)
-print(a)#<zip object at 0x000002F0E1767000>
-#print(list(a))#[(10, 'cat'), (20, 'dor'), (30, 'koh'), (40, 'huh')]
-d=dict(a)
-print(d)#{10: 'cat', 20: 'dor', 30: 'koh', 40: 'huh'}
-
-#使用参数创建字典
-d=dict(cat=10,dog=20)#左侧cat是key,右侧是value
-print(d)
-
-t=(10,20,30)
-print({t:10})#t是key,10是value,元组是可以作为字典中的key
-#字典属于序列
-print('max:',max(d))
-print('min:',min(d))
-print('len:',len(d))
-#del d
-print(d)
+try:
+    num1=int(input('请输入一个整数：'))
+    num2=int(input('请输入另一个整数'))
+    result = num1 / num2
+    print('结果:',result)
+except ZeroDivisionError:
+    print('除数不能为0')
+except ValueError:
+    print('不能将字符串转为整数')
+except BaseException:
+    print('未知异常')
--- damo17.py
+++ damo17.py
@@ -1,19 +1,13 @@
-d={10:'cat',20:'dog',30:'pet',20:'zoo','a':40}
-#访问字典中的元素
-#（1)使用d[key]
-print(d['a'])
-#(2)d.get(key)
-print(d.get('a'))
+try:
+    num1 = int(input('请输入一个整数：'))
+    num2 = int(input('请输入另一个整数'))
+    result = num1 / num2
 
-#二者之间是有区别的，如果key不存在，d[key]报错d.get(key)可以指定默认值
-#print(d['b'])#KeyError: 'b'
-print(d.get('b'))#None
-print(d.get('b','不存在'))
-
-#字典的遍历
-for item in d.items():
-    print(item)#key=value组成的一个元素
-
-#在使用for循环遍历时，分别获得key,value
-for key,value in d.items():
-    print(key,'---->',value)
+except ZeroDivisionError:
+    print('除数不能为0')
+except ValueError:
+    print('不能将字符串转为整数')
+else:
+    print('结果：',result)
+finally:
+    print('程序执行结束!')
