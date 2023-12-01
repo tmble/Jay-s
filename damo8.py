b4220b9f291c156210ad54257a8d07a2d64aba6e
tmble <128164838+tmble@users.noreply.github.com>
2023-12-01 13:40:35 +0800

    Add files via upload

--- damo8.py
+++ damo8.py
@@ -1,20 +1,29 @@
-s='hellodadsffffwe'
-#(1)字符串拼接及not in
-new_s=''
-for item in s:
-    if item not in new_s:
-        new_s+=item#拼接操作
-print(new_s)
+def calc(a,b):
+    return a+b
 
-#(2)使用索引+not in
-new_s2=''
-for i in range(len(s)):
-    if s[i] not in new_s2:
-        new_s2+=s[i]
-print(new_s2)
+print(calc(10,20))
 
-#(3)通过集合去重+列表排序
-new_s3=set(s)
-lst=list(new_s3)
-lst.sort(key=s.index)
-print(''.join(lst))
+#匿名函数
+s=lambda a,b:a+b#s表示就是一个匿名函数
+print(type(s))#<class'function'>
+#调用匿名函数
+print(s(10,20))
+print('-'*30)
+#列表的正常取值操作
+lst=[10,20,30,40,50]
+for i in range(len(lst)):
+    print(lst[i])
+print('-'*30)
+for i in range(len(lst)):
+    result=lambda x:x[i]#根据索引取值，result类型是function，x是形式参数
+    print(result(lst))#lst是实际参数
+print('-'*30)
+student=[
+    {'name':'鹏子一号','score':98},
+    {'name': '鹏子二号', 'score': 97},
+    {'name': '鹏子三号', 'score': 92},
+    {'name': '鹏子四号', 'score': 94}
+]
+#对列表进行排序,排序的规则是字典的成绩
+student.sort(key=lambda x:x.get('score'),reverse=True)#降序
+print(student)
