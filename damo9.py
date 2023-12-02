a1581ed6e23c2b6aa4e8dfd345753df1fb9edd81
tmble <128164838+tmble@users.noreply.github.com>
2023-12-02 15:15:28 +0800

    Add files via upload

--- damo9.py
+++ damo9.py
@@ -1,11 +1,6 @@
-import re
-pattern='\d\.\d+'
-s='I study Python3.11 every day Python2.7 I love you'
-match=re.search(pattern,s)
-print(match)
-
-s2='T study Python every day'
-match2=re.search(pattern,s2)
-print(match2)
-print(match.group())
-#print(match2.group())
+def fac(n):#n的阶乘 N！=N*（N-1)
+    if n==1:
+        return 1
+    else:
+        return n*fac(n-1)#自己调用自己
+print(fac(5))
