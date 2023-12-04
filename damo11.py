54d98d867760ddf51214505ab1ad5b91fc4a87d2
tmble <128164838+tmble@users.noreply.github.com>
2023-12-04 21:29:43 +0800

    Add files via upload

--- damo11.py
+++ damo11.py
@@ -1,10 +1,7 @@
-import re
-pattern='黑客|破解|反爬'
-s='我想学习Python,想破解一些vip视频,Python可以实现无底线反爬吗？'
-new_s=re.sub(pattern,'***',s)
-print(new_s)
-
-s2='https://cn.bing.com/search?q=%E5%88%98%E7%9B%8A%E6%97%AD'
-pattern2='[?|%]'
-lst=re.split(pattern2,s2)
-print(lst)
+print('非空字符串的布尔值：',bool('hello'))#True
+print('空字符串的布尔值：',bool(''))#空字符串不是空格字符串
+print('空列表的布尔值：',bool(list()))
+print('-'*30)
+print('非0数值型的布尔值：',bool(123))
+print('整数0的布尔值：',bool(0))
+print('浮点数0.0的布尔值：',bool(0.0))
