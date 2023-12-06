c504aa22ad02a31fdf64f68cd0072424ac8e7688
tmble <128164838+tmble@users.noreply.github.com>
2023-12-06 21:52:03 +0800

    Add files via upload

--- damo13.py
+++ damo13.py
@@ -1,3 +1,7 @@
-s='HelloPython,HelloJava,helloC++'
-word=input('请输入要统计的字符：')
-print('{0}在{1}一共出现了{2}次'.format(word,s,s.upper().count(word)))
+lst=[54,46,46,4,54,45]
+#(1)排序操作
+asc_lst=sorted(lst)#升序
+desc_lst=sorted(lst,reverse=True)#降序
+print('原列表：',lst)
+print('升序：',asc_lst)
+print('降序：',desc_lst)
